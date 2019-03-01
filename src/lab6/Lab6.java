d400324616bfdb05c0ac5921bacb8a564d71cc95
Jomar <Jomar@134.154.60.189>
2019-03-01 14:59:11 -0800

    Initial Commit clone original

--- src/lab6/Lab6.java
+++ src/lab6/Lab6.java
@@ -16,6 +16,7 @@ public class Lab6 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World original");
         // TODO code application logic here
     }
     
