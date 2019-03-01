1660dccb018794c535765aaee52a24d88a22c0cf
JohannesMendoza <48119259+JohannesMendoza@users.noreply.github.com>
2019-03-01 14:40:07 -0800

    UpdateHelloWorld.Lab6

--- src/lab6/Lab6.java
+++ src/lab6/Lab6.java
@@ -15,6 +15,7 @@ public class Lab6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello World");
         // TODO code application logic here
     }
     
