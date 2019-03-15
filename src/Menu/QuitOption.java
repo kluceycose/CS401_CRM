734f38fbeeee305ae200efa786de6434481d9620
Johannes <johannesmendoza327@gmail.com>
2019-03-15 13:37:38 -0700

    Test

---
+++ src/Menu/QuitOption.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+/**
+ *
+ * @author Jomar
+ */
+public class QuitOption implements MenuItem{
+    @Override
+    public void execute() {
+        System.exit(0);
+    }
+    
+    @Override
+    public String toString() {
+        return "Quit";
+    }
+}
---
+++ src/Menu/ReturnFromMenu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+/**
+ *
+ * @author Jomar
+ */
+public class ReturnFromMenu implements MenuItem{
+     private final String msg;
+    private final MenuStack menus;
+    
+    public ReturnFromMenu(String prevMenuName, MenuStack menuStack) {
+        msg = "Return to " + prevMenuName;
+        menus = menuStack;
+    }
+
+    @Override
+    public void execute() {
+        menus.toPreviousMenu();
+    }
+    
+    @Override
+    public String toString() {
+        return msg;
+    }
+    
+}
